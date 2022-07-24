c7508cd0a34d4cbd7192ade81a7c5ddc1f0cc765
Flokey82 <f.rohrweck@gmail.com>
2022-07-24 14:05:13 +0200

    genetics: Documentation, seed support

--- genetics/genetics.go
+++ genetics/genetics.go
@@ -1,17 +1,54 @@
+// Package genetics provides a simple framework for gene mixing / editing for procedural generation.
 package genetics
 
 import "math/rand"
 
+// Pool provides means to use a set seed for gene generation/mixing.
+type Pool struct {
+	Rand *rand.Rand
+}
+
+// NewPool initializes a gene pool with the given seed.
+func NewPool(seed int64) *Pool {
+	return &Pool{
+		Rand: rand.New(rand.NewSource(seed)),
+	}
+}
+
+// NewRandom returns a randomized set of genes.
+func (p *Pool) NewRandom() Genes {
+	return Genes(p.Rand.Uint64())
+}
+
+// Mix combines the two sets of genes by using the bits in common in both sets
+// as baseline and then randomly sets bits that are different in both sets.
+// Additional a number of mutations can be applied which randomly flip bits.
+func (p *Pool) Mix(a, b Genes, nMutations int) Genes {
+	t := uint64(a) & uint64(b)        // common bits
+	x := (uint64(a) | uint64(b)) &^ t // diff bits
+	t |= x & uint64(p.Rand.Int63())   // random diffs
+
+	// Mutations (random bit flips)
+	for i := 0; i < nMutations; i++ {
+		t ^= 1 << p.Rand.Intn(63)
+	}
+
+	return Genes(t)
+}
+
 type Genes uint64
 
+// NewRandom returns a randomized set of genes.
 func NewRandom() Genes {
 	return Genes(rand.Uint64())
 }
 
+// Set the gene 'd' to the supplied value.
 func (g *Genes) Set(d Gene, val int) {
 	g.setBits(d.Offset, d.NumBits, uint64(val))
 }
 
+// Get the value of gene 'd'.
 func (g *Genes) Get(d Gene) int {
 	return int(g.getBits(d.Offset, d.NumBits))
 }
@@ -28,6 +65,9 @@ func (g *Genes) getBits(offs, n int) uint64 {
 	return (uint64(*g) >> offs) & ((1 << n) - 1)
 }
 
+// Mix combines the two sets of genes by using the bits in common in both sets
+// as baseline and then randomly sets bits that are different in both sets.
+// Additional a number of mutations can be applied which randomly flip bits.
 func Mix(a, b Genes, nMutations int) Genes {
 	t := uint64(a) & uint64(b)        // common bits
 	x := (uint64(a) | uint64(b)) &^ t // diff bits
