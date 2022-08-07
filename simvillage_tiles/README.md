33ebb610b7fb573bf4914cb57d4c0e9acab01a6b
Flokey82 <f.rohrweck@gmail.com>
2022-08-07 14:43:43 +0200

    simvillage_tiles: Multi-layer drawables

--- simvillage_tiles/README.md
+++ simvillage_tiles/README.md
@@ -2,4 +2,23 @@
 
 This package contains a very simple tile based renderer and will hopefully be a nifty village simulation soon.
 
+## Features
+
+* Simple tile based renderer
+* Player controlled character
+* NPC characters (rudimentary)
+* Drawable prefab objects (rudimentary)
+* Collision detection (rudimentary)
+* Chunk loading / chunk generation
+
+## TODO
+
+* Decouple chunk size from viewport size (?)
+* Generation or loading of larger maps
+* Better layer system / named layers
+* Indoor maps
+* Improve tile render order
+* Objects / resources / etc.
+* AI for creatures
+
 ![alt text](https://raw.githubusercontent.com/Flokey82/go_gens/master/simvillage_tiles/images/rgb.png "Screenshot!")
--- simvillage_tiles/tilegen.go
+++ simvillage_tiles/tilegen.go
@@ -105,35 +105,50 @@ var (
 )
 
 func genChunk(x, y, width, height int) [][]int {
+	// Create a new, empty chunk.
 	chunk := make([][]int, 3)
 	for i := range chunk {
 		chunk[i] = make([]int, width*height)
 	}
+
+	// Initialize the random number generator with a unique seed for the given coordinates.
 	r := rand.New(rand.NewSource(cash(x, y)))
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
+			// Fill the ground layer with grass tiles.
+			// NOTE: We've repeated the 243 tile a few times in the grassTiles slice
+			// to skew the probability in favor of the plain grass tile.
 			chunk[0][x+y*width] = grassTiles[r.Intn(len(grassTiles))]
+
+			// Randomly add some flowers on the ground object layer.
 			if r.Intn(100) < 2 {
 				chunk[1][x+y*width] = flowerTiles[r.Intn(len(flowerTiles))]
 			}
 		}
 	}
-	// drawObject draws the given object at (1, 1).
-	drawObject := func(h drawable) {
+
+	// drawObject draws the given object at (dx, dy).
+	drawObject := func(h drawable, dx, dy int) {
 		for x := 0; x < h.width; x++ {
 			for y := 0; y < h.height; y++ {
-				// NOTE: We hardcoded here the start of the house to be
-				// offset by 1 from the top left corner.
-				chunk[2][(x+1)+(y+1)*width] = h.tiles[x+y*h.width]
+				for lIdx, layer := range h.layers {
+					if layer[x+y*h.width] != 0 {
+						chunk[lIdx][(x+dx)+(y+dy)*width] = layer[x+y*h.width]
+					}
+				}
 			}
 		}
 	}
-	// Randomly put some houses somewhere.
+
+	// Randomly put some houses and other objects on the chunk.
+	// NOTE: We currently have hardcoded the offset for the houses to (1, 1).
 	switch r.Intn(10) {
 	case 0:
-		drawObject(house1)
+		drawObject(house1, 1, 1)
 	case 1:
-		drawObject(house2)
+		drawObject(house2, 1, 1)
+	case 2:
+		drawObject(hedge, 1, 1)
 	}
 	return chunk
 }
@@ -146,29 +161,86 @@ func cash(x, y int) int64 {
 	return int64(h ^ (h >> 16))
 }
 
+// drawable is a collection of tiles that can be drawn on a chunk.
+// TODO: Use named layers instead of a fixed number of expected layers.
 type drawable struct {
-	height int
-	width  int
-	tiles  []int
+	height int     // Height of the object.
+	width  int     // Width of the object.
+	layers [][]int // Tiles on each layer
 }
 
 var house1 = drawable{
 	height: 4,
 	width:  4,
-	tiles: []int{
-		58, 59, 60, 61,
-		83, 84, 85, 86,
-		108, 109, 110, 111,
-		133, 134, 135, 136,
+	layers: [][]int{
+		{
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+		},
+		{
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+		},
+		{
+			58, 59, 60, 61,
+			83, 84, 85, 86,
+			108, 109, 110, 111,
+			133, 134, 135, 136,
+		},
 	},
 }
 var house2 = drawable{
 	height: 4,
 	width:  6,
-	tiles: []int{
-		63, 64, 65, 66, 67, 68,
-		88, 89, 90, 91, 92, 93,
-		113, 114, 115, 116, 117, 118,
-		138, 139, 140, 141, 142, 143,
+	layers: [][]int{
+		{
+			0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0,
+		},
+		{
+			0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0,
+		},
+		{
+			63, 64, 65, 66, 67, 68,
+			88, 89, 90, 91, 92, 93,
+			113, 114, 115, 116, 117, 118,
+			138, 139, 140, 141, 142, 143,
+		},
+	},
+}
+var hedge = drawable{
+	height: 5,
+	width:  9,
+	layers: [][]int{
+		{
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 202, 203, 204, 205, 206, 207, 208, 0,
+			0, 227, 228, 229, 230, 231, 232, 233, 0,
+			0, 0, 0, 0, 255, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+		},
+		{
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 280, 0, 0, 0, 0,
+		},
+		{
+			176, 177, 178, 179, 180, 181, 182, 183, 184,
+			201, 0, 0, 0, 0, 0, 0, 0, 209,
+			226, 0, 0, 0, 0, 0, 0, 0, 234,
+			251, 252, 253, 254, 0, 256, 257, 258, 259,
+			276, 277, 278, 279, 0, 281, 282, 283, 284,
+		},
 	},
 }
