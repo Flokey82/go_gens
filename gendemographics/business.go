c249e22becee4eb4ce14a85a36d6910a61a85dc5
Flokey82 <f.rohrweck@gmail.com>
2023-01-02 12:41:38 +0100

    gendemographics: Separate out BusinessTypes

--- gendemographics/business.go
+++ gendemographics/business.go
@@ -30,191 +30,133 @@ func (bt *BusinessType) New() *Business {
 	}
 }
 
-// BusinessTypes is a list of all business types.
-//
 // NOTE: Wouldn't this be better in a CSV or something?
 // On the other hand, it'd be good to have some form of
 // identifier that users of the package can refer to.
 // IDK.
 //
 // ... should it be a map?
-var BusinessTypes = []*BusinessType{{
-	Name:   "Shoemaker",
-	Serves: 150,
-}, {
-	Name:   "Furrier",
-	Serves: 250,
-}, {
-	Name:   "Maidservant",
-	Serves: 250,
-}, {
-	Name:   "Tailor",
-	Serves: 250,
-}, {
-	Name:   "Barber",
-	Serves: 350,
-}, {
-	Name:   "Jeweler",
-	Serves: 400,
-}, {
-	Name:   "Taverns/Restaurant",
-	Serves: 400,
-}, {
-	Name:   "Old-clothes",
-	Serves: 400,
-}, {
-	Name:   "Pastrycook",
-	Serves: 500,
-}, {
-	Name:   "Mason",
-	Serves: 500,
-}, {
-	Name:   "Carpenter",
-	Serves: 550,
-}, {
-	Name:   "Weaver",
-	Serves: 600,
-}, {
-	Name:   "Chandler",
-	Serves: 700,
-}, {
-	Name:   "Mercer",
-	Serves: 700,
-}, {
-	Name:   "Cooper",
-	Serves: 700,
-}, {
-	Name:   "Baker",
-	Serves: 800,
-}, {
-	Name:   "Watercarrier",
-	Serves: 850,
-}, {
-	Name:   "Scabbardmaker",
-	Serves: 850,
-}, {
-	Name:   "Wine-seller",
-	Serves: 900,
-}, {
-	Name:   "Hatmaker",
-	Serves: 950,
-}, {
-	Name:   "Saddler",
-	Serves: 1000,
-}, {
-	Name:   "Chicken Butcher",
-	Serves: 1000,
-}, {
-	Name:   "Pursemaker",
-	Serves: 1100,
-}, {
-	Name:   "Woodseller",
-	Serves: 2400,
-}, {
-	Name:   "Magic-shop",
-	Serves: 2800,
-}, {
-	Name:   "Bookbinder",
-	Serves: 3000,
-}, {
-	Name:   "Butcher",
-	Serves: 1200,
-}, {
-	Name:   "Fishmonger",
-	Serves: 1200,
-}, {
-	Name:   "Beer-seller",
-	Serves: 1400,
-}, {
-	Name:   "Buckle Maker",
-	Serves: 1400,
-}, {
-	Name:   "Plasterer",
-	Serves: 1400,
-}, {
-	Name:   "Spice Merchant",
-	Serves: 1400,
-}, {
-	Name:   "Blacksmith",
-	Serves: 1500,
-}, {
-	Name:   "Painter",
-	Serves: 1500,
-}, {
-	Name:   "Doctor",
-	Serves: 1700,
-}, {
-	Name:   "Roofer",
-	Serves: 1800,
-}, {
-	Name:   "Locksmith",
-	Serves: 1900,
-}, {
-	Name:   "Bather",
-	Serves: 1900,
-}, {
-	Name:   "Ropemaker",
-	Serves: 1900,
-}, {
-	Name:   "Inn",
-	Serves: 2000,
-}, {
-	Name:   "Tanner",
-	Serves: 2000,
-}, {
-	Name:   "Copyist",
-	Serves: 2000,
-}, {
-	Name:   "sculptor",
-	Serves: 2000,
-}, {
-	Name:   "Rugmaker",
-	Serves: 2000,
-}, {
-	Name:   "Harness-Maker",
-	Serves: 2000,
-}, {
-	Name:   "Bleacher",
-	Serves: 2100,
-}, {
-	Name:   "Hay Merchant",
-	Serves: 2300,
-}, {
-	Name:   "Cutler",
-	Serves: 2300,
-}, {
-	Name:   "Glovemaker",
-	Serves: 2400,
-}, {
-	Name:   "Woodcarver",
-	Serves: 2400,
-}, {
-	Name:   "Bookseller",
-	Serves: 6300,
-}, {
-	Name:   "Illuminator",
-	Serves: 3900,
-}, {
-	Name:   "Place Of Worship",
-	Serves: 400,
-},
-	// OTHER JOBS / NOT BUSINESS
-	{
-		Name:   "Law Enforcement",
-		Serves: 150,
-	}, {
-		Name:   "Noble",
-		Serves: 200,
-	}, {
-		Name:   "Administrator",
-		Serves: 650,
-	}, {
-		Name:   "Clergy",
-		Serves: 40,
-	}, {
-		Name:   "Priest",
-		Serves: 1000,
-	},
+var (
+	BizShoeMaker      = &BusinessType{Name: "Shoemaker", Serves: 150}
+	BizFurrier        = &BusinessType{Name: "Furrier", Serves: 250}
+	BizMaidservant    = &BusinessType{Name: "Maidservant", Serves: 250}
+	BizTailor         = &BusinessType{Name: "Tailor", Serves: 250}
+	BizBarber         = &BusinessType{Name: "Barber", Serves: 350}
+	BizJeweler        = &BusinessType{Name: "Jeweler", Serves: 400}
+	BizTavern         = &BusinessType{Name: "Taverns/Restaurant", Serves: 400}
+	BizOldClothes     = &BusinessType{Name: "Old-clothes", Serves: 400}
+	BizPastryCook     = &BusinessType{Name: "Pastrycook", Serves: 500}
+	BizMason          = &BusinessType{Name: "Mason", Serves: 500}
+	BizCarpenter      = &BusinessType{Name: "Carpenter", Serves: 550}
+	BizWeaver         = &BusinessType{Name: "Weaver", Serves: 600}
+	BizChandler       = &BusinessType{Name: "Chandler", Serves: 700}
+	BizMercer         = &BusinessType{Name: "Mercer", Serves: 700}
+	BizCooper         = &BusinessType{Name: "Cooper", Serves: 700}
+	BizBaker          = &BusinessType{Name: "Baker", Serves: 800}
+	BizWaterCarrier   = &BusinessType{Name: "Water carrier", Serves: 850}
+	BizScabbardmaker  = &BusinessType{Name: "Scabbardmaker", Serves: 850}
+	BizWineSeller     = &BusinessType{Name: "Wine-seller", Serves: 900}
+	BizHatmaker       = &BusinessType{Name: "Hatmaker", Serves: 950}
+	BizSaddler        = &BusinessType{Name: "Saddler", Serves: 1000}
+	BizChickenButcher = &BusinessType{Name: "Chicken Butcher", Serves: 1000}
+	BizPursemaker     = &BusinessType{Name: "Pursemaker", Serves: 1100}
+	BizWoodseller     = &BusinessType{Name: "Woodseller", Serves: 2400}
+	BizMagicShop      = &BusinessType{Name: "Magic-shop", Serves: 2800}
+	BizBookbinder     = &BusinessType{Name: "Bookbinder", Serves: 3000}
+	BizButcher        = &BusinessType{Name: "Butcher", Serves: 1200}
+	BizFishmonger     = &BusinessType{Name: "Fishmonger", Serves: 1200}
+	BizBeerSeller     = &BusinessType{Name: "Beer-seller", Serves: 1400}
+	BizBucklemaker    = &BusinessType{Name: "Bucklemaker", Serves: 1400}
+	BizPlasterer      = &BusinessType{Name: "Plasterer", Serves: 1400}
+	BizSpiceMerchant  = &BusinessType{Name: "Spice merchant", Serves: 1400}
+	BizBlacksmith     = &BusinessType{Name: "Blacksmith", Serves: 1500}
+	BizPainter        = &BusinessType{Name: "Painter", Serves: 1500}
+	BizDoctor         = &BusinessType{Name: "Doctor", Serves: 1700}
+	BizRoofer         = &BusinessType{Name: "Roofer", Serves: 1800}
+	BizLocksmith      = &BusinessType{Name: "Locksmith", Serves: 1900}
+	BizBather         = &BusinessType{Name: "Bather", Serves: 1900}
+	BizRopemaker      = &BusinessType{Name: "Ropemaker", Serves: 1900}
+	BizInn            = &BusinessType{Name: "Inn", Serves: 2000}
+	BizTanner         = &BusinessType{Name: "Tanner", Serves: 2000}
+	BizCopyist        = &BusinessType{Name: "Copyist", Serves: 2000}
+	BizSculptor       = &BusinessType{Name: "Sculptor", Serves: 2000}
+	BizRugmaker       = &BusinessType{Name: "Rugmaker", Serves: 2000}
+	BizHarnessMaker   = &BusinessType{Name: "Harness-maker", Serves: 2000}
+	BizBleacher       = &BusinessType{Name: "Bleacher", Serves: 2100}
+	BizHayMerchant    = &BusinessType{Name: "Hay merchant", Serves: 2300}
+	BizCutler         = &BusinessType{Name: "Cutler", Serves: 2300}
+	BizGlovemaker     = &BusinessType{Name: "Glovemaker", Serves: 2400}
+	BizWoodcarver     = &BusinessType{Name: "Woodcarver", Serves: 2400}
+	BizBookseller     = &BusinessType{Name: "Bookseller", Serves: 6300}
+	BizIlluminator    = &BusinessType{Name: "Illuminator", Serves: 3900}
+	BizPlaceOfWorship = &BusinessType{Name: "Place of Worship", Serves: 400}
+	BizLawEnforcement = &BusinessType{Name: "Law Enforcement", Serves: 150}
+	BizNoble          = &BusinessType{Name: "Noble", Serves: 200}
+	BizAdministrator  = &BusinessType{Name: "Administrator", Serves: 650}
+	BizClergy         = &BusinessType{Name: "Clergy", Serves: 40}
+	BizPriest         = &BusinessType{Name: "Priest", Serves: 1000}
+)
+
+// BusinessTypes is a list of all business types.
+var BusinessTypes = []*BusinessType{
+	BizShoeMaker,
+	BizFurrier,
+	BizMaidservant,
+	BizTailor,
+	BizBarber,
+	BizJeweler,
+	BizTavern,
+	BizOldClothes,
+	BizPastryCook,
+	BizMason,
+	BizCarpenter,
+	BizWeaver,
+	BizChandler,
+	BizMercer,
+	BizCooper,
+	BizBaker,
+	BizWaterCarrier,
+	BizScabbardmaker,
+	BizWineSeller,
+	BizHatmaker,
+	BizSaddler,
+	BizChickenButcher,
+	BizPursemaker,
+	BizWoodseller,
+	BizMagicShop,
+	BizBookbinder,
+	BizButcher,
+	BizFishmonger,
+	BizBeerSeller,
+	BizBucklemaker,
+	BizPlasterer,
+	BizSpiceMerchant,
+	BizBlacksmith,
+	BizPainter,
+	BizDoctor,
+	BizRoofer,
+	BizLocksmith,
+	BizBather,
+	BizRopemaker,
+	BizInn,
+	BizTanner,
+	BizCopyist,
+	BizSculptor,
+	BizRugmaker,
+	BizHarnessMaker,
+	BizBleacher,
+	BizHayMerchant,
+	BizCutler,
+	BizGlovemaker,
+	BizWoodcarver,
+	BizBookseller,
+	BizIlluminator,
+	BizPlaceOfWorship,
+	BizLawEnforcement,
+	BizNoble,
+	BizAdministrator,
+	BizClergy,
+	BizPriest,
 }
 
 /* ORIGINAL LIST
