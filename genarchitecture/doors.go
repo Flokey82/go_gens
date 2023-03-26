c65526ca44a087afb55aa227123cb557bffaa8bd
Flokey82 <f.rohrweck@gmail.com>
2023-03-26 17:14:49 +0200

    genarchitecture: Basic door rendering to SVG

--- genarchitecture/doors.go
+++ genarchitecture/doors.go
@@ -1,8 +1,6 @@
 package genarchitecture
 
 import (
-	"math"
-
 	svg "github.com/ajstarks/svgo"
 )
 
@@ -10,102 +8,57 @@ type DoorStyle struct {
 	Shape string
 	Size  int
 	BaseStyle
-	// TODO: Handles, hinges, locks, etc.
+	// TODO: Handles, hinges, locks, frame etc.
 }
 
 func (s DoorStyle) Description() string {
-	return "A " + s.Shape + " door " + s.BaseStyle.Description()
+	return s.Shape + " shaped " + s.BaseStyle.Description()
 }
 
+const doorOuterStyle = "fill:white;stroke:black;stroke-width:2"
+
 func (s DoorStyle) DrawToSVG(sv *svg.SVG, x, y, width, height int) {
-	switch s.Shape {
+	marginFrame := 16
+	drawShape(sv, x, y, width, height, randomString(shapeDoors))
+	drawShape(sv, x, y, width, height, s.Shape)
+	drawShape(sv, x+marginFrame/2, y+marginFrame/2, width-marginFrame, height-marginFrame, s.Shape)
+}
+
+func drawShape(sv *svg.SVG, x, y, width, height int, shape string) {
+	switch shape {
 	case ShapeRectangle:
-		sv.Rect(x, y, width, height)
-	case ShapeCircle:
-		sv.Circle(x+width/2, y+height/2, height/2)
+		sv.Rect(x, y, width, height, doorOuterStyle)
 	case ShapeTrapazoid:
-		var points [][2]int
-		topWidth := width / 2
-		points = append(points, [2]int{x, y + height})
-		points = append(points, [2]int{x + width, y + height})
-		points = append(points, [2]int{x + topWidth + (width-topWidth)/2, y})
-		points = append(points, [2]int{x + (width-topWidth)/2, y})
-
-		var x, y []int
-		for _, p := range points {
-			x = append(x, p[0])
-			y = append(y, p[1])
-		}
-
-		sv.Polygon(x, y)
+		points := genTrapezoid(x, y, width, height)
+		x1, y1 := convertToPairSlices(points)
+		sv.Polygon(x1, y1, doorOuterStyle)
 	case ShapeOval:
-		sv.Ellipse(x+width/2, y+height/2, width/2, height/2)
+		sv.Ellipse(x+width/2, y+height/2, width/2, height/2, doorOuterStyle)
 	case ShapeTriangle:
-		var points [][2]int
-		points = append(points, [2]int{x + width/2, y})
-		points = append(points, [2]int{x + width, y + height})
-		points = append(points, [2]int{x, y + height})
-
-		var x, y []int
-		for _, p := range points {
-			x = append(x, p[0])
-			y = append(y, p[1])
-		}
-
-		sv.Polygon(x, y)
+		points := genTriangle(x, y, width, height)
+		x1, y1 := convertToPairSlices(points)
+		sv.Polygon(x1, y1, doorOuterStyle)
 	case ShapeHexagon:
-		var points [][2]int
-		points = append(points, [2]int{x, y})
-		points = append(points, [2]int{x + width, y})
-		points = append(points, [2]int{x + width + width/2, y + height/2})
-		points = append(points, [2]int{x + width, y + height})
-		points = append(points, [2]int{x, y + height})
-		points = append(points, [2]int{x - width/2, y + height/2})
-
-		var x, y []int
-		for _, p := range points {
-			x = append(x, p[0])
-			y = append(y, p[1])
-		}
-
-		sv.Polygon(x, y)
+		points := genHexagon(x, y, width, height)
+		x1, y1 := convertToPairSlices(points)
+		sv.Polygon(x1, y1, doorOuterStyle)
 	case ShapeOctagon:
-		var points [][2]int
-
-		// Draw the 8 sides of the octagon
-		for i := 0; i < 8; i++ {
-			points = append(points, [2]int{
-				x + width/2 + int(float64(width/2)*math.Cos(float64(i)*math.Pi/4+math.Pi/8)),
-				y + height/2 + int(float64(height/2)*math.Sin(float64(i)*math.Pi/4+math.Pi/8)),
-			})
-		}
-
-		var x, y []int
-		for _, p := range points {
-			x = append(x, p[0])
-			y = append(y, p[1])
-		}
-
-		sv.Polygon(x, y)
+		points := genOctagon(x, y, width, height)
+		x1, y1 := convertToPairSlices(points)
+		sv.Polygon(x1, y1, doorOuterStyle)
 	case ShapeArch:
 		// A simple arch like a door arc (part of an oval)
-		sv.Arc(x, y+height, width/2, height, 0, false, true, x+width, y+height)
+		sv.Arc(x, y+height, width/2, height, 0, false, true, x+width, y+height, doorOuterStyle)
 	}
-
 }
 
 var shapeDoors = []string{
 	ShapeRectangle,
-	ShapeCircle,
 	ShapeTrapazoid,
 	ShapeOval,
 	ShapeTriangle,
 	ShapeHexagon,
 	ShapeOctagon,
-	ShapeArch,
-	ShapeArch,
-	ShapeArch,
-	ShapeArch,
 	ShapeArch,
 }
 
--- genarchitecture/shape.go
+++ genarchitecture/shape.go
@@ -1,5 +1,7 @@
 package genarchitecture
 
+import "math"
+
 const (
 	ShapeRectangle = "rectangle"
 	ShapeCircle    = "circle"
@@ -10,3 +12,67 @@ const (
 	ShapeOctagon   = "octagon"
 	ShapeArch      = "arch"
 )
+
+func genHexagon(x, y, width, height int) [][2]int {
+	var points [][2]int
+	points = append(points, [2]int{x + width/3, y})
+	points = append(points, [2]int{x + width - width/3, y})
+	points = append(points, [2]int{x + width, y + height/2})
+	points = append(points, [2]int{x + width - width/3, y + height})
+	points = append(points, [2]int{x + width/3, y + height})
+	points = append(points, [2]int{x, y + height/2})
+	return points
+}
+
+func genTriangle(x, y, width, height int) [][2]int {
+	var points [][2]int
+	points = append(points, [2]int{x + width/2, y})
+	points = append(points, [2]int{x + width, y + height})
+	points = append(points, [2]int{x, y + height})
+	return points
+}
+
+func genTrapezoid(x, y, width, height int) [][2]int {
+	var points [][2]int
+	topWidth := width / 2
+	points = append(points, [2]int{x, y + height})
+	points = append(points, [2]int{x + width, y + height})
+	points = append(points, [2]int{x + topWidth + (width-topWidth)/2, y})
+	points = append(points, [2]int{x + (width-topWidth)/2, y})
+	return points
+}
+
+func genOctagon(x, y, width, height int) [][2]int {
+	var points [][2]int
+	offsetAngle := math.Pi / 8
+
+	// Draw the 8 sides of the octagon
+	for i := 0; i < 8; i++ {
+		points = append(points, [2]int{
+			x + width/2 + int(float64(width/2)*math.Cos(float64(i)*math.Pi/4+offsetAngle)),
+			y + height/2 + int(float64(height/2)*math.Sin(float64(i)*math.Pi/4+offsetAngle)),
+		})
+	}
+
+	return points
+}
+
+func genCircle(x, y, width, height, nSteps int) [][2]int {
+	var points [][2]int
+	for i := 0; i < nSteps; i++ {
+		points = append(points, [2]int{
+			x + width/2 + int(float64(width/2)*math.Cos(float64(i)*math.Pi/float64(nSteps))),
+			y + height/2 + int(float64(height/2)*math.Sin(float64(i)*math.Pi/float64(nSteps))),
+		})
+	}
+	return points
+}
+
+func convertToPairSlices(points [][2]int) ([]int, []int) {
+	var x, y []int
+	for _, p := range points {
+		x = append(x, p[0])
+		y = append(y, p[1])
+	}
+	return x, y
+}
