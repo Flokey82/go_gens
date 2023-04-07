a2de121942608feb7abb60de900951c2cb265417
Flokey82 <f.rohrweck@gmail.com>
2023-04-07 19:10:06 +0200

    gengeometry: add Bresenham line algo

--- gengeometry/blockyshape.go
+++ gengeometry/blockyshape.go
@@ -365,3 +365,68 @@ var BlockyShapes = []BlockyShape{
 	BlockyTShape,
 	BlockyPlusShape,
 }
+
+// GetBresenhamLine returns a list of points that are on the line between pos1 and pos2.
+// The line is drawn using the Bresenham algorithm.
+// See: http://www.roguebasin.com/index.php/Bresenham%27s_Line_Algorithm
+func GetBresenhamLine(pos1, pos2 vectors.IVec2) (points []vectors.IVec2) {
+	x1, y1 := pos1.X, pos1.Y
+	x2, y2 := pos2.X, pos2.Y
+
+	isSteep := abs(y2-y1) > abs(x2-x1)
+	if isSteep {
+		x1, y1 = y1, x1
+		x2, y2 = y2, x2
+	}
+
+	reversed := false
+	if x1 > x2 {
+		x1, x2 = x2, x1
+		y1, y2 = y2, y1
+		reversed = true
+	}
+
+	deltaX := x2 - x1
+	deltaY := abs(y2 - y1)
+	err := deltaX / 2
+	y := y1
+	var ystep int64
+
+	if y1 < y2 {
+		ystep = 1
+	} else {
+		ystep = -1
+	}
+
+	for x := x1; x < x2+1; x++ {
+		if isSteep {
+			points = append(points, vectors.IVec2{X: y, Y: x})
+		} else {
+			points = append(points, vectors.IVec2{X: x, Y: y})
+		}
+		err -= deltaY
+		if err < 0 {
+			y += ystep
+			err += deltaX
+		}
+	}
+
+	if reversed {
+		//Reverse the slice
+		for i, j := 0, len(points)-1; i < j; i, j = i+1, j-1 {
+			points[i], points[j] = points[j], points[i]
+		}
+	}
+
+	return
+}
+
+func abs(x int64) int64 {
+	switch {
+	case x < 0:
+		return -x
+	case x == 0:
+		return 0
+	}
+	return x
+}
